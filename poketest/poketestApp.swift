37c1f83a9bfe1f5d13fef83101074f08e9755785
kocho31 <99873300+kocho31@users.noreply.github.com>
2025-03-11 15:07:22 +0000

    Initial Commit

---
+++ poketest/poketestApp.swift
@@ -0,0 +1,17 @@
+//
+//  poketestApp.swift
+//  poketest
+//
+//  Created by Alexandre Martins on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct poketestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
