faf47e5bbd7d86b519611716a27b09b8eda25719
kocho31 <99873300+kocho31@users.noreply.github.com>
2025-03-11 16:23:15 +0000

    Api-Call-Listing

--- poketest/ContentView.swift
+++ poketest/ContentView.swift
@@ -8,17 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pokemons: [Pokemons] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List (pokemons, id: \.name) { pokemon in
+                Text(pokemon.name)
+            }
         }
         .padding()
+        .task {
+            do{
+                pokemons = try await getPokemon()
+            } catch PokeErros.invalidURL {
+                print("invalid url")
+            } catch PokeErros.invalidResponse {
+                print("invalid response")
+            } catch PokeErros.invalidData {
+                print("invalid data")
+            } catch {
+                print("unexpectet error")
+            }
+        }
     }
+    
+    func getPokemon() async throws -> [Pokemons]{
+        let endpoint = "https://pokeapi.co/api/v2/pokemon/"
+        
+        guard let url = URL(string: endpoint) else {
+            throw PokeErros.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode==200 else{
+            throw PokeErros.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let apiResponse = try decoder.decode(PokeTestApi.self, from: data)
+            return apiResponse.results
+        } catch {
+            throw PokeErros.invalidData
+        }
+        
+    }
+    
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct PokeTestApi: Codable{
+    let count: Int?
+    let next: String?
+    let previous: String?
+    let results: [Pokemons]
+}
+
+struct Pokemons: Codable {
+    let name: String
+    let url: String
+}
+
+enum PokeErros:Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
